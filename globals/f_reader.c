45a50a17aadb00970fabaf3283ff024d8d866cc5
Keegan Fontaine <keegsfontaine@gmail.com>
2024-12-04 21:25:08 -0500

    Fixed global c file reading module

--- globals/f_reader.c
+++ globals/f_reader.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 // Yes I do inline functions in C. Sue me.
-char* pzle(char* filename, int buffersize) {
+char* pzle(char* filename, int buffer_size, int line_count) {
     FILE* fptr;
     fptr = fopen(filename, "r");
 
-    char str_buff[buffersize];
+    char str_buff[buffer_size];
 
     if (fptr == NULL) {
         char* err = "File Pointer Was Null! Does the file actually exist?";
@@ -16,10 +16,10 @@ char* pzle(char* filename, int buffersize) {
         fclose(fptr);
         return err;
     } else {
-        char* buff = malloc(1000 * buffersize * sizeof(char));
+        char* buff = malloc(line_count * buffer_size * sizeof(char));
 
         int i = 0;
-        while (fgets(str_buff, buffersize, fptr)) {
+        while (fgets(str_buff, buffer_size, fptr)) {
             for (int j = 0; str_buff[j]; j++) {
                 buff[i] = str_buff[j];
 
--- globals/f_reader.h
+++ globals/f_reader.h
@@ -1,6 +1,6 @@
 #ifndef F_READER_H
 #define F_READER_H
 
-char* pzle(char* filename, int buffersize);
+char* pzle(char* filename, int buffer_size, int line_count);
 
 #endif
