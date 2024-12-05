830a3272c4e99f1e969e5b41eadd02ccf21870e4
Keegan Fontaine <keegsfontaine@gmail.com>
2024-12-04 20:36:34 -0500

    First puzzle finished
    cred to Geeks For Geeks (https://www.geeksforgeeks.org/c-bubble-sort/) for the bubble sort implementation

--- 1/main.c
+++ 1/main.c
@@ -1,13 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../globals/f_reader.h"
 
-int main() {
-    char* val = pzle("input.txt", 100);
+void swap(int* arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
 
-    for (int i = 0; val[i]; i++) {
-        printf("%c", val[i]);
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(arr, j, j + 1);
+        }
+    }
+}
+
+int main() {
+    int line_size = 5;
+    int spaces_between_columns = 3;
+
+    int buffer_size = line_size * 2 + spaces_between_columns + 1;
+    int line_count = 1000;
+
+    char* val = pzle("input.txt", buffer_size, line_count);
+
+    int left_list[line_count];
+
+    for (int j = 0; j < line_count; j++) {
+        char line_str[line_size + 1];
+
+        for (int i = 0; val[i] != ' '; i++) {
+            line_str[i] = val[i + buffer_size * j];
+        }
+
+        left_list[j] = atoi(line_str);
     }
 
+    int right_list[line_count];
+
+    for (int j = 0; j < line_count; j++) {
+        char line_str[line_size + 1];
+        int x = 0;
+
+        for (int i = 8; val[i] != '\n'; i++) {
+            line_str[x] = val[i + buffer_size * j];
+
+            x++;
+        }
+
+        right_list[j] = atoi(line_str);
+    }
+
+    // Literally everything above this is project setup (I'm bad at C)
+
+    bubble_sort(right_list, line_count);
+    bubble_sort(left_list, line_count);
+
+    int sum = 0;
+    for (int i = 0; i < line_count; i++) {
+        int diff = right_list[i] - left_list[i];
+
+        if (diff < 0) diff *= -1;
+
+        sum += diff;
+    }
+
+    free(val);
+    printf("Output: %d\n", sum);
     return 0;
 }
